54c053f3cae51f1992d9d6b728bcc68418ccdd9d
Carlos <adrian.ag666@gmail.com>
2021-03-06 01:52:07 -0600

    Genera-marcadores-mapa-con-texto-en-popup-variable

--- generarMapa.py
+++ generarMapa.py
@@ -4,6 +4,7 @@ import pandas
 data = pandas.read_csv("Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
+elev = list(data["ELEV"])
 #Crea un mapa auntando a las coordenadas indicadas  zoom_start indica el nivel de zoom con el que se va a abrir el mapa
 map = folium.Map(location=[20.52, -103.38], zoom_start=6, tiles="Stamen Terrain")
 #para ver el tipo que se genera
@@ -12,8 +13,8 @@ map = folium.Map(location=[20.52, -103.38], zoom_start=6, tiles="Stamen Terrain"
 #Agregar marcadores  al mapa, popup es el mensaje que se muestra en el punto 
 fg = folium.FeatureGroup(name="My Map")
 #la función zip()  permite el poder iterar sobre las dos listas al mismo tiempo
-for lt, ln  in zip(lat,lon):
-    fg.add_child(folium.Marker(location= [lt, ln], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+for lt, ln, el  in zip(lat,lon,elev):
+    fg.add_child(folium.Marker(location= [lt, ln], popup=str(el) + "m", icon=folium.Icon(color='green')))
 
 
 
