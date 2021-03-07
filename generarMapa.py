ed286de2ae1515ec458d045a96d2048b898772b4
Carlos <adrian.ag666@gmail.com>
2021-03-07 00:42:32 -0600

    Agregando-HTML-al-Popup

--- generarMapa.py
+++ generarMapa.py
@@ -5,7 +5,13 @@ data = pandas.read_csv("Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
+
+html = """<h4>Volcano information:</h4>
+    Height: %s m
+    """
+
 #Crea un mapa auntando a las coordenadas indicadas  zoom_start indica el nivel de zoom con el que se va a abrir el mapa
+
 map = folium.Map(location=[20.52, -103.38], zoom_start=6, tiles="Stamen Terrain")
 #para ver el tipo que se genera
 #map
@@ -13,11 +19,12 @@ map = folium.Map(location=[20.52, -103.38], zoom_start=6, tiles="Stamen Terrain"
 #Agregar marcadores  al mapa, popup es el mensaje que se muestra en el punto 
 fg = folium.FeatureGroup(name="My Map")
 #la función zip()  permite el poder iterar sobre las dos listas al mismo tiempo
-for lt, ln, el  in zip(lat,lon,elev):
-    fg.add_child(folium.Marker(location= [lt, ln], popup=str(el) + "m", icon=folium.Icon(color='green')))
+for lt, ln, el in zip(lat, lon, elev):
+        iframe = folium.IFrame(html=html % str(el), width=200, height=100)
+        fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon = folium.Icon(color = "green")))
 
 
 
 map.add_child(fg)
 #crea el HTML con el mapa
-map.save("map1.html")
+map.save("Map_html_popup_simple.html")
